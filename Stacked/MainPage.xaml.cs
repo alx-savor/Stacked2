f05b5d95f6e996395710f3fb9d72b9972cf053b4
alexander.savorwest <alexander.savorwest@5CD2226TL7>
2025-04-03 13:53:03 +0200

    Add project files.

---
+++ Stacked/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Stacked
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
---
+++ Stacked/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Stacked;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
